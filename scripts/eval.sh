be063c7dd81bd881fd637dc8422b1e7455e680d0
LiaoMengqi <1799859708@qq.com>
2025-08-26 15:20:03 +0800

    rm

--- scripts/eval.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python evaluation/eval_math.py \
-    --exp_name "evaluation" \
-    --output_dir "." \
-    --base_dir "./results" \
-    --dataset gsm8k
--- scripts/test.sh
+++
@@ -1,9 +0,0 @@
-
-PARAM_VALUES=(256 1024 1536 2048 )
-
-for val in "${PARAM_VALUES[@]}"
-do
-  echo $val
-done
-
-echo finish
